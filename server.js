27003ccebd3f6ce989713fa9729f4528d8dabdcc
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2020-11-27 14:04:03 -0500

    WIP; Server is not serving up Client

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(() => console.log(`Server is listening on port: ${PORT}!`));
