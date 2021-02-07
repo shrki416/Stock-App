e7c6146df17c6a7490aa429e9149b77721beb40e
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-02-06 21:06:39 -0500

    Reset App

--- server.js
+++ server.js
@@ -1,15 +1,1 @@
-const express = require("express");
-const path = require("path");
 
-require("dotenv").config();
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, "client/build")));
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
-
-app.listen(() => console.log(`Server is listening on port: ${PORT}!`));
